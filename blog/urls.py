56f018f01aece542000c5aa2fb755e0db8c20174
mekoness <mekoness@gmail.com>
2023-01-04 17:14:54 -0800

    Added view and temp for detailed blog post

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.post_list, name="posts_list")
+    path("", views.post_list, name="posts_list"),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
